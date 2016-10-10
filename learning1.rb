512e9e8db31273f8e5dca795222646b9edb45789
Victoriosu <seb.echeverria@alumnos.duoc.cl>
2016-10-10 00:38:59 -0300

    Watch out, there's a spider on the loose

--- learning1.rb
+++ learning1.rb
@@ -34,9 +34,38 @@ class Perro < Animal
 		puts("#{nombre} is a #{$globalVar} and he is a #{family}" )
 	end
 end
+
+class Araña < Animal
+	attr_accessor :size, :country
+
+	def initialize(size, country, family, legs, eyes)
+		@size=size
+		@country=country
+		super(family, legs, eyes)
+	end
+
+	def printData()
+		puts('    
+     |
+     |
+     |
+     |
+     |
+  /  |   \
+ ;_/,L-,\_;
+\._/3  E\_./
+\_./(::)\._/
+     ''')
+		puts("\nThis is an arachnid specimen and its data is\n #{inspect}")
+	end
+end
+
 #Execution of methods.
+=begin
 perro = Perro.new('Spunky', 'Yorkshire','canine', 4, 2)
 p(perro)
 perro.printSentence
 perro.countEyes
-
+=end
+spider = Araña.new('3 cms.','Spain', 'Arachnid', 8, 6)
+spider.printData
