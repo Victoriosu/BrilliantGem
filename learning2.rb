0633796fc077c9458ae9fcdaf3933808dcd9a4c8
Victoriosu <seb.echeverria@alumnos.duoc.cl>
2016-10-15 14:13:43 -0300

    A more aggresive handling on arrays

---
+++ learning2.rb
@@ -0,0 +1,29 @@
+class Employee
+	
+	attr_accesor :name, :age, :social_n
+	
+
+	#Method to set data on an employee
+	def data()
+		print('Please enter the name of the employee')
+		@name=gets.chomp
+		print('Now the age')
+		@age=gets.chomp	
+		print('Finally his social number')
+		@social_n=gets.chomp
+	end
+	
+	#Method to set data over the entire employee's array.
+
+class Registry
+
+	employee_a=[]
+
+	#Method to know how long will the array be
+	def fill()
+		for i in employee_a
+			i=gets.chomp
+		print('Do you wish to add another
+	end
+
+end
