3439b7576e488ec09da6115422224dea6683de12
Victoriosu <seb.echeverria@alumnos.duoc.cl>
2016-10-22 20:27:16 -0300

    Getting to work

--- learning2.rb
+++ learning2.rb
@@ -1,29 +1,37 @@
 class Employee
 	
-	attr_accesor :name, :age, :social_n
+	attr_accessor :name, :age, :social_n
 	
 
+	def initialize(aName,anAge, aSocial_n)
+		@name=aName
+		@age=anAge
+		@social_n=aSocial_n
+	end
 	#Method to set data on an employee
 	def data()
-		print('Please enter the name of the employee')
-		@name=gets.chomp
-		print('Now the age')
-		@age=gets.chomp	
-		print('Finally his social number')
-		@social_n=gets.chomp
+		puts("Please enter the name of the employee")
+		@name = gets
+		puts("Now the age")
+		@age = gets
+		puts("Finally his social number")
+		@social_n = gets
 	end
 	
 	#Method to set data over the entire employee's array.
+end
 
 class Registry
-
-	employee_a=[]
-
-	#Method to know how long will the array be
-	def fill()
-		for i in employee_a
-			i=gets.chomp
-		print('Do you wish to add another
+	def showData(array)
+		for i in 0..3
+			puts(array[i])
+		end
 	end
-
 end
+reg1=Registry.new
+employee1=Employee.new('Juan',18,'18956241-1')
+employee2=Employee.new('Juan',18,'18956241-1')
+employee3=Employee.new('Juan',18,'18956241-1')
+employee4=Employee.new('Juan',18,'18956241-1')
+employee_a=[employee1,employee2,employee3,employee4]
+reg1.showData(employee_a)
